69fed03898283a8b601db284bfbbf09fc9639615
olanikegloria github <olanikegloria2020@gmail.com>
2023-06-30 09:13:18 +0100

    fix linters

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import './style.css';
 import loadMovies from './modules/displayItems.js';
-<<<<<<< HEAD
 import movieCount from './modules/movieCount.js';
 
 loadMovies();
 movieCount();
-=======
-import openPopup from './modules/popup.js';
-
-loadMovies();
-
-document.addEventListener('DOMContentLoaded', () => {
-  openPopup();
-});
->>>>>>> dev
